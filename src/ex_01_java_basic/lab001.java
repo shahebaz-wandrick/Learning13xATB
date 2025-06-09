25ce608b87579d8f394ff3aaa5372ca20c66b884
test_1 <shahebaz.wandrick@hotmail.com>
2025-06-09 05:04:04 +0300

    Update

---
+++ src/ex_01_java_basic/lab001.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package ex_01_java_basic;
+
+public class lab001 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+
+        }
+    }
